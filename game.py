52b338b859a53e13905fb3256178fee194726a6b
Siddhant Deshmukh <91955718+siddhantdeshmukh-cyber@users.noreply.github.com>
2022-08-31 09:42:55 -0500

    Create game.py

---
+++ game.py
@@ -0,0 +1,11 @@
+secret_no=9
+guess_count=0
+guess_limit=3
+while guess_count<=guess_limit:
+    guess = int(input('guess: '))
+    guess_count+=1
+    if guess==secret_no:
+        print('winer')
+        break
+else:
+    print('failed')
